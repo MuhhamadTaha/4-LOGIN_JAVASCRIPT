313250cc1335b017bbc8f18e043e29f5ab67d837
Muhhamadtaha <muhhamadtahas@gmail.com>
2025-01-29 17:29:13 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function myfunction(){
+            
+    var uname = document.getElementById("uname").value;
+    var upass = document.getElementById("upass").value;
+
+    if ( uname == "mts766"  &&  upass == 766123
+        
+       ) {
+        
+        alert(" you sign in susscessfully.......! ")
+
+    }else{
+
+        alert("Invalid username and password");
+    }
+
+}
